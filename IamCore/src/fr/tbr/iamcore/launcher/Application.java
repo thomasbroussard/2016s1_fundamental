613729a315630019956a70d1c8513b5f4e56f31d
tbrou <tbrou@tbrou-PC7.natsystem.fr>
2016-05-24 20:24:55 +0200

    * add: IdentityFileDAO integration

--- IamCore/src/fr/tbr/iamcore/launcher/Application.java
+++ IamCore/src/fr/tbr/iamcore/launcher/Application.java
@@ -3,9 +3,12 @@
  */
 package fr.tbr.iamcore.launcher;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import fr.tbr.iamcore.datamodel.Identity;
 import fr.tbr.iamcore.service.authentication.AuthenticationService;
+import fr.tbr.iamcore.service.dao.IdentityFileDAO;
 
 /**
  * @author tbrou
@@ -15,24 +18,25 @@ public class Application {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws IOException {
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to the IAM System");
-		
+
 		System.out.println("Please enter your user name: ");
 		String username = scanner.nextLine();
 		System.out.println("Please enter your passord: ");
 		String password = scanner.nextLine();
-		
+
 		AuthenticationService authService = new AuthenticationService();
-		if (!authService.authenticate(username,password)){
+		if (!authService.authenticate(username, password)) {
 			System.out.println("The provided credentials are wrong");
 			scanner.close();
 			return;
 		}
-		
+		IdentityFileDAO dao = new IdentityFileDAO("/tests/identities.txt");
 		System.out.println("Menu for the IAM application :");
 		System.out.println("1 - Create an Identity");
 		System.out.println("2 - Update an Identity");
@@ -43,16 +47,22 @@ public class Application {
 		switch (menuAnswer) {
 		case "1":
 			System.out.println("Creation Activity");
-			
-			
-			
-			
-			
+
+			System.out.print("Please enter the Identity display name");
+			String displayName = scanner.nextLine();
+			System.out.print("Please enter the Identity email address");
+			String email = scanner.nextLine();
+			System.out.print("Please enter the Identity uid");
+			String uid = scanner.nextLine();
+			Identity identity = new Identity(displayName, email, uid);
+
+			dao.save(identity);
+
 			break;
 		case "2":
 			System.out.println("Modification Activity");
 			break;
-			
+
 		case "3":
 			System.out.println("Deletion Activity");
 			break;
@@ -60,14 +70,11 @@ public class Application {
 			System.out.println("The program will now exit");
 			scanner.close();
 			return;
-			
+
 		default:
 			break;
 		}
-		
-	
-		
-		
+
 		scanner.close();
 	}
 
