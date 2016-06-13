100b285b1a99435b02701ca723fbe19b46692c53
tbrou <tbrou@tbrou-PC7.mshome.net>
2016-06-13 19:38:39 +0200

    *upd : completed the JDBCDAO

--- IamCore/src/fr/tbr/iamcore/exception/DAOExceptionsMessages.java
+++ IamCore/src/fr/tbr/iamcore/exception/DAOExceptionsMessages.java
@@ -4,6 +4,7 @@ public enum DAOExceptionsMessages {
 
 		UNABLE_TO_READ_XML_FILE("unable to read the identities.xml file","XML01"),
 		UNABLE_TO_READ_TEXT_FILE("unable to read the text file", "TXT01"),
+		UNABLETOCONNECT ("unable to connect to the database", "DTB01"),
 		
 		;
 		
--- IamCore/src/fr/tbr/iamcore/service/dao/IdentityJDBCDAO.java
+++ IamCore/src/fr/tbr/iamcore/service/dao/IdentityJDBCDAO.java
@@ -12,45 +12,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.tbr.iamcore.datamodel.Identity;
+import fr.tbr.iamcore.exception.DAOExceptionsMessages;
 import fr.tbr.iamcore.exception.DAOInitializationException;
 import fr.tbr.iamcore.exception.DAOSearchException;
 
 /**
- * This class is dealing with the Identity Persistence
- * using a JDBC back-end
+ * This class is dealing with the Identity Persistence using a JDBC back-end
+ * 
  * @author tbrou
  *
  */
 public class IdentityJDBCDAO {
-	
-	
-	public List<Identity> search(Identity criteria) throws DAOSearchException{
-		List<Identity> identities = new ArrayList<>(); 
-		
-		Connection connection;
+
+	private static final String QUERY_ALL_IDENTITIES = "select * from IDENTITIES";
+	private static final String COLUMN_IDENTITY_DISPLAYNAME = "IDENTITY_DISPLAYNAME";
+	private static final String CONF_USERNAME = "tom";
+	private static final String CONF_PASSWORD = "tom";
+	private static final String CONF_CONNECTION_STRING = "jdbc:derby://localhost:1527/Identities;create=true";
+	private Connection connection;
+
+	public IdentityJDBCDAO() throws DAOInitializationException {
 		try {
-			connection = DriverManager.getConnection("jdbc:derby://localhost:1527/Identities;create=true"
-					, "tom", "tom");
+			this.connection = getConnection();
+		} catch (SQLException e) {
+			DAOInitializationException die = new DAOInitializationException(
+					DAOExceptionsMessages.UNABLETOCONNECT);
+			die.initCause(e);
+			throw die;
+		}
+
+	}
+
+	public List<Identity> search(Identity criteria) throws DAOSearchException {
+		List<Identity> identities = new ArrayList<>();
+		try {
+
+			if (connection.isClosed()) {
+				this.connection = getConnection();
+			}
+			// prepare the query
+			PreparedStatement prepareStatement = connection
+					.prepareStatement(QUERY_ALL_IDENTITIES);
+			ResultSet rs = prepareStatement.executeQuery();
+			while (rs.next()) {
+				String displayName = rs.getString(COLUMN_IDENTITY_DISPLAYNAME);
+				System.out.println(displayName);
+			}
+
 		} catch (SQLException e) {
 			DAOSearchException searchException = new DAOSearchException();
 			searchException.initCause(e);
 			throw searchException;
 		}
-
-		//prepare the query
-		PreparedStatement prepareStatement = connection.prepareStatement("select * from IDENTITIES");
-		ResultSet rs = prepareStatement.executeQuery();
-		while(rs.next()){
-			String displayName = rs.getString("IDENTITY_DISPLAYNAME");
-			System.out.println(displayName);
-		}
-		
-		connection.close();
-		
 		return identities;
-		
-		
+
 	}
-	
+
+	private Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(
+				CONF_CONNECTION_STRING,
+				CONF_USERNAME, CONF_PASSWORD);
+	}
 
 }
